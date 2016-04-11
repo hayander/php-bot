db2ad0306a9c40eeabcad54265457d38d7d3b227
Hayden Anderson <hayander@gmail.com>
2016-04-12 01:57:50 +1000

    Class autoloader implemented

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
-/*
- * PHP Bot System
- * hayander
- * 2016-04-11
- */
+    /*
+     * PHP Bot entry point
+     * hayander
+     * 2016-04-11
+     */
+
+    try {
+
+        require 'src/loader.php';
+
+        spl_autoload_register( 'Loader::load' );
+
+        if (file_exists( __DIR__ . '/config.php' )) {
+            $config = include_once( __DIR__ . '/config.php' );
+        }
+
+    } catch (Exception $e) {
+        echo( 'FATAL ERROR: ' . $e->getMessage() . PHP_EOL );
+    }
